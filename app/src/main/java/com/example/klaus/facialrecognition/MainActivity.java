1660b1c33cadbab6255f9df2cc4e3d8a2e6b3ed5
ivanspajic <ivan.spajic@gmail.com>
2016-05-12 00:43:59 +0200

    first commit

--- app/src/main/java/com/example/klaus/facialrecognition/MainActivity.java
+++ app/src/main/java/com/example/klaus/facialrecognition/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.klaus.facialrecognition;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private ImageView myImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImageView = (ImageView) findViewById(R.id.imageView);
+        myImageView.setImageResource(R.drawable.cat);
     }
 }
